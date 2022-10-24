bd341ca18be2fb2e94d0780030232c06efa8c334
Aaron Elkiss <aelkiss@umich.edu>
2022-10-24 14:56:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ echo "⚙️ making the '_default' confisgset available'"
 docker-compose run --rm solr bash -c "mkdir -v /var/solr/data/configsets; cp -vrp /opt/solr/server/solr/configsets/_default /var/solr/data/configsets"
 
 echo "💎 installing gems"
-docker-compose run --rm ruby bundle install
+docker-compose run --rm query bundle install
 
 echo "🏃 starting solr (docker-compose logs -f solr to see what's happening)"
 docker-compose up -d solr
